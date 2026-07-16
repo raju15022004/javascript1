9d72c5e42c15094105c97b6ee20aa463aaf61213
raju15022004 <raju032004@gmail.com>
2026-07-16 15:20:22 +0530

    array.js

---
+++ Collage/array.js
@@ -0,0 +1,17 @@
+// arr=[1,6,8,3];
+// // n=arr.length;
+// console.log(arr);
+// arr.push(9);
+// console.log(arr);
+// arr.push(0);
+// console.log(arr);
+// arr.pop(0);
+// console.log(arr);
+// arr.unshift(2);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+
+arr=[4,"raju",7.9,true]
+console.log(arr);
