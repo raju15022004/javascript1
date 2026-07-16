8e8c9fc73f966502c42cb717e0e20a18d34ad1d6
raju15022004 <raju032004@gmail.com>
2026-07-16 15:00:11 +0530

    conditional.js

---
+++ Collage/conditional.js
@@ -0,0 +1,14 @@
+rating=5;
+
+// 1-Poor
+// 2-Below Average
+// 3-Average
+// 4-Good
+// 5-Exellent
+
+if(rating==1) console.log('Poor');
+else if(rating==2) console.log('Below Average');
+else if(rating==3) console.log(' Average');
+else if(rating==4) console.log('Good');
+else console.log('Exellent');
+
