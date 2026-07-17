e4bfbc0ed74706f270dbdb3ff2d35a5e63150c56
raju15022004 <raju032004@gmail.com>
2026-07-17 13:50:49 +0530

    String.js

---
+++ Collage/String.js
@@ -0,0 +1,17 @@
+// let s="raju";
+// console.log(s);
+// s.toUpperCase();
+// let s2=s.toUpperCase();
+// console.log(s,s2);
+
+
+// let s="Raju Kumar";
+// console.log(s.slice(5));
+// console.log(s.slice(2,6));
+
+let s="raju is a teacher at physics wallah";
+console.log(s);
+let arr=s.split(' ');
+for(const ele of arr){
+  console.log(ele);
+}
