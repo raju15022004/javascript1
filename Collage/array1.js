602fd42bf357f0bfe9bd677773a4907d24c0ed9b
raju15022004 <raju032004@gmail.com>
2026-07-17 13:29:00 +0530

    array.js

---
+++ Collage/array1.js
@@ -0,0 +1,37 @@
+// let fruits=["Apple","Banana","Mango"];
+// console.log(fruits);
+
+//create array
+
+// let numbers=[10,20,30,40];
+// console.log(numbers);
+
+// Using the array Constructor
+// let numbers=new Array(10,20,30);
+// console.log(numbers);
+
+// Accessing Elements
+
+// let fruits=["Apple","Banna","Mango"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+
+//Updating Elements
+// let fruits=["Apple","Banana","Mango"];
+// fruits[1]="orange";
+
+// console.log(fruits);
+
+// length
+
+// let numbers=[10,20,30];
+// console.log(numbers.length);
+
+// let arr=["A","B","c"];
+// console.log(arr.indexOf("B"))
+
+// let arr=[10,20,30];
+// console.log(arr.includes(20));
+
+let arr=[10,20,30,40,50];
+console.log(arr.slice(1,4));
