d649f65b35c4b7549b4e95328d1f74a651f75767
raju15022004 <raju032004@gmail.com>
2026-07-19 18:06:52 +0530

    function.js

---
+++ Collage/function.js
@@ -0,0 +1,13 @@
+// console.log(Math.abs(-7));
+// console.log(Math.max(5,1));
+
+// console.log(Math.min(-5,1));
+
+console.log(Math.pow(3.14));
+console.log(Math.sqrt(3.14));
+
+// console.log(Math.floor(-3.5));
+
+console.log(Math.random());
+
+
