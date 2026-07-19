5e75432545aa49e87e6b4522a2df9b26df543fae
raju15022004 <raju032004@gmail.com>
2026-07-20 00:48:17 +0530

    math.js

--- Collage/math.js
+++ Collage/math.js
@@ -1,2 +1,12 @@
-let randomNumber=Math.floor(Math.random() * 11);
-console.log(randomNumber);
+// let randomNumber=Math.floor(Math.random() * 11);
+// console.log(randomNumber);
+
+// function getRandomNumber(){
+//   return Math.floor(Math.random() * 11.0);
+// }
+// console.log(getRandomNumber());
+
+function getRandomNumber(min,max){
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+console.log(getRandomNumber(0,10));
