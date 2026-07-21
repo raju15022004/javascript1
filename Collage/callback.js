7fc30c955aca2dbf3572a29721a233fc25c9013b
raju15022004 <raju032004@gmail.com>
2026-07-21 14:11:25 +0530

    setTime.js

--- Collage/callback.js
+++ Collage/callback.js
@@ -1,10 +1,22 @@
+// function product(a,b,c){
+//   return a*b*c;
+// }
+
+// function fun(x,y){
+// console.log(x-y);
+
+// }
+
+// fun(product(3,8,9),7);
+
+
 function product(a,b,c){
   return a*b*c;
 }
 
 function fun(x,y){
-console.log(x-y);
-
+  let a=x(2,5,4);
+  console.log(a-y);
 }
 
-fun(product(3,8,9),7);
+fun(product,7);
---
+++ Collage/setTime.js
@@ -0,0 +1,8 @@
+function hello(){
+  console.log("hello");
+}
+function mello(){
+  console.log("mello");
+}
+setTimeout(hello,2*1000);
+setTimeout(mello,1*1000);
