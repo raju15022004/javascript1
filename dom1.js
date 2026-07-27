3eb43c8162bc6fdc9fa79d060b0a5d8dd9259e0d
raju15022004 <raju032004@gmail.com>
2026-07-28 00:48:42 +0530

    dom1.js

---
+++ dom1.js
@@ -0,0 +1,17 @@
+let x=document.getElementById("ele1")
+x.addEventListener("click",function(){
+  x.style.color="yellow";
+  x.style.backgroundColor="red";
+})
+
+let y=document.getElementById("ele2")
+y.addEventListener("mousemove",function(){
+  y.style.color="white";
+  y.style.backgroundColor="orange";
+})
+
+let z=document.getElementById("ele3")
+z.addEventListener("mouseenter",function(){
+  z.style.color="black ";
+  z.style.backgroundColor="red"
+})
